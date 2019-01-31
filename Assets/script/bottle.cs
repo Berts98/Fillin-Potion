6b01f2db145c25dd6c733593fccf72f74613e678
Berts98 <bertolani.anna@gmail.com>
2019-01-31 18:03:32 +0100

    teleport

---
+++ Assets/script/bottle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class bottle : MonoBehaviour {
+
+    public Image emptyImage;
+
+    public Image fillImage;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
